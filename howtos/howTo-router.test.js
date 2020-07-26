7e60e7a263a34de7b75907bb953b910b3109deea
meloegel <meloegel@gmail.com>
2020-07-26 12:04:29 -0400

    finished tests

--- howtos/howTo-router.test.js
+++ howtos/howTo-router.test.js
@@ -6,11 +6,11 @@ const db = require('../data/db-config')
 
 describe('how-To end point tests', function () {
     describe('GET tests', function () {
-        beforeAll(async () => {
+        beforeEach(async () => {
             await db('howto').truncate()
             await db('users').truncate()
         })
-        it('howto/ should return status 200', async () => {
+        it('GET howto/ should return status 200 and respond with JSON', async () => {
             let token = ''
             await supertest(server)
                 .post('/api/auth/register')
@@ -24,17 +24,25 @@ describe('how-To end point tests', function () {
                     expect(res.type).toMatch(/json/i)
                 })
         })
-        // it('howto/1 should return status 200', function () {
-        //     return supertest(server)
-        //         .get('/api/auth/howto/1')
-        //         .then(res => {
-        //             expect(res.status).toBe(200)
-        //         })
-        // })
-        it('howto/1 should return json', function () {
+        it('GET howto/1 should return status 200 and respond with json', async () => {
+            let token = ''
+            await supertest(server)
+                .post('/api/auth/register')
+                .send({ username: 'test', password: 'test', role: 2 })
+                .then(async result => token = result.body.token)
+            await supertest(server)
+                .post('/api/auth/login')
+                .send({ username: 'test', password: 'test' })
+                .then(async result => token = result.body.jwt_token)
+            await supertest(server)
+                .post('/api/auth/howto/creator')
+                .send({ name: 'test', description: 'test', steps: 'test', category: 'test' })
+                .set({ authorization: token })
             return supertest(server)
                 .get('/api/auth/howto/1')
+                .set({ authorization: token })
                 .then(res => {
+                    expect(res.status).toBe(200)
                     expect(res.type).toMatch(/json/i)
                 })
         })
--- howtos/howToCreator-router.test.js
+++ howtos/howToCreator-router.test.js
@@ -5,19 +5,69 @@ const db = require('../data/db-config')
 
 
 describe('Creator end point tests', function () {
-    describe('Get tests', function () {
-        beforeAll(async () => {
+    describe('Creator tests', function () {
+        beforeEach(async () => {
             await db('howto').truncate()
             await db('users').truncate()
         })
-        it('user/ should return status 200', async () => {
+        it('POST creator/ should return status 201 and respond with json', async () => {
             let token = ''
             await supertest(server)
                 .post('/api/auth/register')
                 .send({ username: 'test', password: 'test', role: 2 })
                 .then(async result => token = result.body.token)
+            await supertest(server)
+                .post('/api/auth/login')
+                .send({ username: 'test', password: 'test' })
+                .then(async result => token = result.body.jwt_token)
             return supertest(server)
-                .get('/api/auth/users')
+                .post('/api/auth/howto/creator')
+                .send({ name: 'test 2', description: 'test', steps: 'test', category: 'test' })
+                .set({ authorization: token })
+                .then(res => {
+                    expect(res.status).toBe(201)
+                    expect(res.type).toMatch(/json/i)
+                })
+        })
+        it('PUT creator/1 should return status 200 and respond with json', async () => {
+            let token = ''
+            await supertest(server)
+                .post('/api/auth/register')
+                .send({ username: 'test', password: 'test', role: 2 })
+                .then(async result => token = result.body.token)
+            await supertest(server)
+                .post('/api/auth/login')
+                .send({ username: 'test', password: 'test' })
+                .then(async result => token = result.body.jwt_token)
+            await supertest(server)
+                .post('/api/auth/howto/creator')
+                .send({ name: 'test 3', description: 'test', steps: 'test', category: 'test' })
+                .set({ authorization: token })
+            return supertest(server)
+                .put('/api/auth/howto/creator/1')
+                .send({ id: 1, name: 'test updated', description: 'test', steps: 'test', category: 'test' })
+                .set({ authorization: token })
+                .then(res => {
+                    expect(res.status).toBe(200)
+                    expect(res.type).toMatch(/json/i)
+                })
+        })
+        it('DELETE creator/1 should return status 200 and respond with json', async () => {
+            let token = ''
+            await supertest(server)
+                .post('/api/auth/register')
+                .send({ username: 'test', password: 'test', role: 2 })
+                .then(async result => token = result.body.token)
+            await supertest(server)
+                .post('/api/auth/login')
+                .send({ username: 'test', password: 'test' })
+                .then(async result => token = result.body.jwt_token)
+            await supertest(server)
+                .post('/api/auth/howto/creator')
+                .send({ name: 'test 1', description: 'test', steps: 'test', category: 'test' })
+                .set({ authorization: token })
+            return supertest(server)
+                .delete('/api/auth/howto/creator/1')
                 .set({ authorization: token })
                 .then(res => {
                     expect(res.status).toBe(200)
