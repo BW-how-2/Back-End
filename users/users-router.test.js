5efffa34e333d538e19026eafb0a4f7baf7aa8a6
meloegel <meloegel@gmail.com>
2020-07-26 11:17:12 -0400

    finished user tests

--- users/users-router.test.js
+++ users/users-router.test.js
@@ -6,11 +6,11 @@ const db = require('../data/db-config')
 
 describe('User end point tests', function () {
     describe('Get tests', function () {
-        beforeAll(async () => {
+        beforeEach(async () => {
             await db('howto').truncate()
             await db('users').truncate()
         })
-        it('user/ should return status 200', async () => {
+        it('GET users/ should return status 200 and response should be JSON', async () => {
             let token = ''
             await supertest(server)
                 .post('/api/auth/register')
@@ -22,6 +22,53 @@ describe('User end point tests', function () {
                 .then(res => {
                     expect(res.status).toBe(200)
                     expect(res.type).toMatch(/json/i)
+                })
+        })
+        it('GET users/1 should return status 200 and response should be JSON', async () => {
+            let token = ''
+            await supertest(server)
+                .post('/api/auth/register')
+                .send({ username: 'test', password: 'test', role: 2 })
+                .then(async result => token = result.body.token)
+            return supertest(server)
+                .get('/api/auth/users/1')
+                .set({ authorization: token })
+                .then(res => {
+                    expect(res.status).toBe(200)
+                    expect(res.type).toMatch(/json/i)
+                })
+        })
+    })
+    describe('PUT & DELETE tests', function () {
+        it('PUT users/1 should return status 200, response should be JSON and respond when user is updated', async () => {
+            let token = ''
+            await supertest(server)
+                .post('/api/auth/register')
+                .send({ username: 'test', password: 'test', role: 2 })
+                .then(async result => token = result.body.token)
+            await supertest(server)
+                .put('/api/auth/users/1')
+                .send({ username: 'testupdate', password: 'test', role: 2 })
+                .set({ authorization: token })
+                .then(res => {
+                    expect(res.status).toBe(200)
+                    expect(res.type).toMatch(/json/i)
+                    expect(res.body).toMatchObject({ Message: "Updated User" })
+                })
+        })
+        it('DELETE users/1 should return status 200, response should be JSON and respond when user has been deleted', async () => {
+            let token = ''
+            await supertest(server)
+                .post('/api/auth/register')
+                .send({ username: 'test', password: 'test', role: 2 })
+                .then(async result => token = result.body.token)
+            await supertest(server)
+                .delete('/api/auth/users/1')
+                .set({ authorization: token })
+                .then(res => {
+                    expect(res.status).toBe(200)
+                    expect(res.type).toMatch(/json/i)
+                    expect(res.body).toMatchObject({ Removed: "User with id: 1" })
                 })
         })
     })
